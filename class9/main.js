2bea8a6ed9c37e4eeb3285c52aee2dc1b7e44572
anwarshah <syed.anwar@nextgeni.dev>
2020-05-11 01:00:33 +0500

    js basics class 9

--- class9/main.js
+++ class9/main.js
@@ -1,13 +1,34 @@
-var a = 1;
-document.write(a);
-var x = "Please Enter Number";
-var y = "Enter Number";
-var a = +prompt(x,y);
-var b = 25 + a;
-var score = 0;
-if(b>30){
-    score++;
-    console.log("Answer is : " + b + " your score is : "+score);
-}
+// var a = 1;
+// document.write(a);
+// var x = "Please Enter Number";
+// var y = "Enter Number";
+// var a = +prompt(x,y);
+// var b = 25 + a;
+// var score = 0;
+// if(b>30){
+//     score++;
+//     console.log("Answer is : " + b + " your score is : "+score);
+// }
 
- 
+//remove from front
+
+// var array = [];
+// array.push("Syed Anwar Ahmed Shah",25);
+// array.unshift("shah","shah");
+// console.log(array);
+
+//remove from last
+
+// var array = [];
+
+// array.push("Syed Anwar Ahmed Shah",25);
+// array.pop();
+// console.log(array);
+
+//splice 
+
+var arr = ['shah',1,'shah','shahji','shahsab',1];
+
+console.log(arr);
+var copy = arr.slice(0,3);
+console.log(copy);
