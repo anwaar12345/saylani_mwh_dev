798044bfd3dceb40cc67b854adee960d929ee196
anwarshah <syed.anwar@nextgeni.dev>
2020-05-10 23:28:11 +0500

    Js Started From Basics

---
+++ class9/main.js
@@ -0,0 +1,13 @@
+var a = 1;
+document.write(a);
+var x = "Please Enter Number";
+var y = "Enter Number";
+var a = +prompt(x,y);
+var b = 25 + a;
+var score = 0;
+if(b>30){
+    score++;
+    console.log("Answer is : " + b + " your score is : "+score);
+}
+
+ 
